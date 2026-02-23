00c1911789866a10c8789226a2b387ef5fb5ab76
Andriana Politi <adapoliti10@gmail.com>
2026-02-23 23:20:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const weddingDate = new Date("July 9, 2026 18:00:00").getTime();
+
+const timer = setInterval(function () {
+
+  const now = new Date().getTime();
+  const distance = weddingDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
+
+}, 1000);
